af62201513ffcdd9689822047f1867aa5188115d
Qin99113 <464859643@qq.com>
2024-11-15 18:45:43 -0500

    Update latency trace

---
+++ scratch/tcp_newreno_latency.cc
@@ -0,0 +1,170 @@
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/flow-monitor-module.h"
+#include <map>
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("TcpExample");
+
+std::string dir;
+std::map<uint64_t, Time> packetSentTimestamps; // Map to store packet sent timestamps
+uint32_t prev = 0;
+Time prevTime = Seconds(0);
+
+// Callback for sending packets
+void PacketSentCallback(Ptr<const Packet> packet)
+{
+    uint64_t uid = packet->GetUid(); // Get the unique ID of the packet
+    packetSentTimestamps[uid] = Simulator::Now(); // Record the sending time
+}
+
+// Callback for receiving packets
+void PacketReceivedCallback(Ptr<const Packet> packet)
+{
+    uint64_t uid = packet->GetUid(); // Get the unique ID of the packet
+    auto it = packetSentTimestamps.find(uid);
+
+    if (it != packetSentTimestamps.end())
+    {
+        Time sentTime = it->second; // Get the sending time
+        Time latency = Simulator::Now() - sentTime; // Calculate latency
+
+        std::ofstream latFile(dir + "packet-latency.dat", std::ios::out | std::ios::app);
+        latFile << uid << " " << latency.GetSeconds() << std::endl; // Log the latency
+        latFile.close();
+
+        packetSentTimestamps.erase(it); // Remove the record to save memory
+    }
+}
+
+// Calculate throughput
+void TraceThroughput(Ptr<FlowMonitor> monitor)
+{
+    FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
+    auto itr = stats.begin();
+    Time curTime = Now();
+    std::ofstream thr(dir + "throughput.dat", std::ios::out | std::ios::app);
+    thr << curTime << " "
+        << 8 * (itr->second.txBytes - prev) /
+               (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds()))
+        << std::endl;
+    prevTime = curTime;
+    prev = itr->second.txBytes;
+    Simulator::Schedule(Seconds(0.2), &TraceThroughput, monitor);
+}
+
+// Trace congestion window
+void CwndTracer(Ptr<OutputStreamWrapper> stream, uint32_t oldval, uint32_t newval)
+{
+    *stream->GetStream() << Simulator::Now().GetSeconds() << " " << newval / 1448.0 << std::endl;
+}
+
+void TraceCwnd(uint32_t nodeId, uint32_t socketId)
+{
+    AsciiTraceHelper ascii;
+    Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream(dir + "cwnd.dat");
+    Config::ConnectWithoutContext("/NodeList/" + std::to_string(nodeId) +
+                                  "/$ns3::TcpL4Protocol/SocketList/" +
+                                  std::to_string(socketId) + "/CongestionWindow",
+                                  MakeBoundCallback(&CwndTracer, stream));
+}
+
+int main(int argc, char* argv[])
+{
+    time_t rawtime;
+    struct tm* timeinfo;
+    char buffer[80];
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(buffer, sizeof(buffer), "%d-%m-%Y-%I-%M-%S", timeinfo);
+    std::string currentTime(buffer);
+
+    // Set the default TCP Congestion Control Protocol to NewReno
+    Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::TcpNewReno"));
+
+    // Command line parser for simulation parameters
+    CommandLine cmd(__FILE__);
+    cmd.Parse(argc, argv);
+
+    // Set the resolution for time to nanoseconds
+    Time::SetResolution(Time::NS);
+
+    // Enable logging for PacketSink and OnOffApplication
+    LogComponentEnable("PacketSink", LOG_LEVEL_INFO);
+    LogComponentEnable("OnOffApplication", LOG_LEVEL_INFO);
+
+    // Create nodes for the network
+    NodeContainer nodes;
+    nodes.Create(2); // Two nodes (n0: Sender, n1: Receiver)
+
+    // Setup point-to-point connection attributes
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    // Install devices on the nodes
+    NetDeviceContainer devices = pointToPoint.Install(nodes);
+
+    // Install the Internet stack on the nodes
+    InternetStackHelper stack;
+    stack.Install(nodes);
+
+    // Assign IP addresses
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces = address.Assign(devices);
+
+    // Set up TCP server and client applications
+    uint16_t port = 8080;
+    Address localAddress(InetSocketAddress(Ipv4Address::GetAny(), port));
+
+    // PacketSink (Receiver) on Node 1
+    PacketSinkHelper packetSinkHelper("ns3::TcpSocketFactory", localAddress);
+    ApplicationContainer serverApps = packetSinkHelper.Install(nodes.Get(1));
+    serverApps.Start(Seconds(0.0));
+    serverApps.Stop(Seconds(10.0));
+
+    // OnOffApplication (Sender) on Node 0
+    OnOffHelper clientHelper("ns3::TcpSocketFactory", Address());
+    clientHelper.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+    clientHelper.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
+    clientHelper.SetAttribute("DataRate", DataRateValue(DataRate("5Mbps")));
+    clientHelper.SetAttribute("PacketSize", UintegerValue(1024));
+    AddressValue remoteAddress(InetSocketAddress(interfaces.GetAddress(1), port));
+    clientHelper.SetAttribute("Remote", remoteAddress);
+
+    ApplicationContainer clientApps = clientHelper.Install(nodes.Get(0));
+    clientApps.Start(Seconds(0.1));
+    clientApps.Stop(Seconds(10.0));
+
+    // Set up trace callbacks
+    devices.Get(0)->TraceConnectWithoutContext("PhyTxEnd", MakeCallback(&PacketSentCallback));
+    devices.Get(1)->TraceConnectWithoutContext("PhyRxEnd", MakeCallback(&PacketReceivedCallback));
+
+    // Schedule congestion window tracing
+    Simulator::Schedule(Seconds(0.1) + MilliSeconds(1), &TraceCwnd, 0, 0);
+
+    // Flow Monitor for throughput calculation
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+    Simulator::Schedule(Seconds(0.2), &TraceThroughput, monitor);
+
+    // Create a new directory to store the output of the program
+    dir = "tcp_newreno_point_results/" + currentTime + "/";
+    std::string dirToSave = "mkdir -p " + dir;
+    if (system(dirToSave.c_str()) == -1)
+    {
+        exit(1);
+    }
+
+    // Run the simulation
+    Simulator::Stop(Seconds(10.1));
+    Simulator::Run();
+    Simulator::Destroy();
+
+    return 0;
+}
