04cb09245220bb9b31d5040a20404760ce360533
Yufei Fiona Wang <39022636+elffiona@users.noreply.github.com>
2024-12-05 19:19:22 -0500

    Adding multiple sender topology

--- tcp_llm_multi_sender.cc
+++ tcp_llm_multi_sender.cc
@@ -21,9 +21,9 @@
 
 // This program simulates the following topology:
 //
-//           1000 Mbps           10Mbps          1000 Mbps
-//  Sender -------------- R1 -------------- R2 -------------- Receiver
-//              5ms               10ms               5ms
+//               1000 Mbps           10Mbps          1000 Mbps
+//  Sender * 3 -------------- R1 -------------- R2 -------------- Receiver
+//                  5ms               10ms               5ms
 //
 // The link between R1 and R2 is a bottleneck link with 10 Mbps. All other
 // links are 1000 Mbps.
@@ -63,23 +63,103 @@
 using namespace ns3;
 
 std::string dir;
-uint32_t prev = 0;
+std::map<uint32_t, uint64_t> prevFlowTxBytes;
+// uint32_t prev = 0;
 Time prevTime = Seconds(0);
+
+void InitializePrevFlowTxBytes(Ptr<FlowMonitor> monitor)
+{
+    FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
+    for (const auto& stat : stats)
+    {
+        std::cout << "Hey I got here" << std::endl;
+        uint32_t flowId = stat.first;
+        prevFlowTxBytes[flowId] = 0; // Initialize to 0
+    }
+}
+
+// // Calculate throughput
+// static void
+// TraceThroughput(Ptr<FlowMonitor> monitor, Ptr<Ipv4FlowClassifier> classifier)
+// {
+//     // FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
+//     // auto itr = stats.begin();
+//     // Time curTime = Now();
+//     // std::ofstream thr(dir + "/throughput.dat", std::ios::out | std::ios::app);
+//     // thr << curTime << " "
+//     //     << 8 * (itr->second.txBytes - prev) /
+//     //            (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds()))
+//     //     << std::endl;
+//     // prevTime = curTime;
+//     // prev = itr->second.txBytes;
+//     // Simulator::Schedule(Seconds(0.2), &TraceThroughput, monitor);
+
+//     FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
+//     Time curTime = Now();
+//     std::ofstream thr(dir + "/throughput.dat", std::ios::out | std::ios::app);
+
+//     for (const auto& stat : stats)
+//     {
+//         uint32_t flowId = stat.first; // Flow ID
+
+//         // Get source and destination addresses using the classifier
+//         Ipv4FlowClassifier::FiveTuple fiveTuple = classifier->FindFlow(flowId);
+//         std::cout << "Flow ID: " << flowId << " Source: ";
+//         fiveTuple.sourceAddress.Print(std::cout);
+//         std::cout << " Destination: ";
+//         fiveTuple.destinationAddress.Print(std::cout);
+//         std::cout << std::endl;
+        
+//         const FlowMonitor::FlowStats& flowStats = stat.second;
+//         thr << "Flow " << flowId << " at time " << curTime.GetSeconds() << " s: "
+//             << 8 * (flowStats.txBytes - prevFlowTxBytes[flowId]) /
+//                    (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds()))
+//             << " Mbps" << std::endl;
+
+//         // Update previous values for this flow
+//         prevFlowTxBytes[flowId] = flowStats.txBytes;
+//     }
+//     prevTime = curTime;
+//     Simulator::Schedule(Seconds(0.2), &TraceThroughput, monitor, classifier);
+// }
 
 // Calculate throughput
 static void
 TraceThroughput(Ptr<FlowMonitor> monitor)
 {
+    // FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
+    // auto itr = stats.begin();
+    // Time curTime = Now();
+    // std::ofstream thr(dir + "/throughput.dat", std::ios::out | std::ios::app);
+    // thr << curTime << " "
+    //     << 8 * (itr->second.txBytes - prev) /
+    //            (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds()))
+    //     << std::endl;
+    // prevTime = curTime;
+    // prev = itr->second.txBytes;
+    // Simulator::Schedule(Seconds(0.2), &TraceThroughput, monitor);
+
     FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
-    auto itr = stats.begin();
     Time curTime = Now();
     std::ofstream thr(dir + "/throughput.dat", std::ios::out | std::ios::app);
-    thr << curTime << " "
-        << 8 * (itr->second.txBytes - prev) /
-               (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds()))
-        << std::endl;
+
+    for (const auto& stat : stats)
+    {
+        uint32_t flowId = stat.first; // Flow ID
+        // Only consider flows 1, 2, and 3
+        if (0 < flowId && flowId < 4)
+        {
+            const FlowMonitor::FlowStats& flowStats = stat.second;
+            thr << "Flow " << flowId << " at time " << curTime.GetSeconds() << " s: "
+                << 8 * (flowStats.txBytes - prevFlowTxBytes[flowId]) /
+                        (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds()))
+                << " Mbps" << std::endl;
+
+            // Update previous values for this flow
+            prevFlowTxBytes[flowId] = flowStats.txBytes;
+        }
+    }
     prevTime = curTime;
-    prev = itr->second.txBytes;
     Simulator::Schedule(Seconds(0.2), &TraceThroughput, monitor);
 }
 
@@ -105,7 +185,7 @@ void
 TraceCwnd(uint32_t nodeId, uint32_t socketId)
 {
     AsciiTraceHelper ascii;
-    Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream(dir + "/cwnd.dat");
+    Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream(dir + "cwnd_" + std::to_string(nodeId) + ".dat");
     Config::ConnectWithoutContext("/NodeList/" + std::to_string(nodeId) +
                                       "/$ns3::TcpL4Protocol/SocketList/" +
                                       std::to_string(socketId) + "/CongestionWindow",
@@ -121,7 +201,7 @@ void RttTracer(Ptr<OutputStreamWrapper> stream, ns3::Time oldval, ns3::Time newv
 void TraceRtt(uint32_t nodeId, uint32_t socketId)
 {
     AsciiTraceHelper ascii;
-    Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream(dir + "rtt.dat");
+    Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream(dir + "rtt_" + std::to_string(nodeId) + ".dat");
     Config::ConnectWithoutContext("/NodeList/" + std::to_string(nodeId) +
                                   "/$ns3::TcpL4Protocol/SocketList/" +
                                   std::to_string(socketId) + "/RTT",
@@ -159,6 +239,7 @@ main(int argc, char* argv[])
     queueDisc = std::string("ns3::") + queueDisc;
 
     std::string tcpVariant = "TcpLlm"; 
+    // std::string tcpVariant = "TcpNewReno"; 
     tcpVariant = std::string ("ns3::") + tcpVariant;
     Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TypeId::LookupByName (tcpVariant)));
 
@@ -171,30 +252,60 @@ main(int argc, char* argv[])
     Config::SetDefault("ns3::DropTailQueue<Packet>::MaxSize", QueueSizeValue(QueueSize("1p")));
     Config::SetDefault(queueDisc + "::MaxSize", QueueSizeValue(QueueSize("100p")));
 
-    NodeContainer sender;
+    // Modified
+    // Sender: 0, 1, 2, Receiver: 3, Router: 4, 5
+    NodeContainer senders;
     NodeContainer receiver;
     NodeContainer routers;
-    sender.Create(1);
+    senders.Create(3);
+    // uint32_t nNodes = senders.GetN();
+    // for (uint32_t i = 0; i < nNodes; ++i)
+    // {
+    //     Ptr<Node> p = senders.Get(i);
+    //     std::cout << "Node " << p->GetId() << std::endl;  // some Node method
+    // }
+
     receiver.Create(1);
+    // uint32_t nNodes1 = receiver.GetN();
+    // for (uint32_t i = 0; i < nNodes1; ++i)
+    // {
+    //     Ptr<Node> p = receiver.Get(i);
+    //     std::cout << "Node " << p->GetId() << std::endl;  // some Node method
+    // }
+
     routers.Create(2);
+    // uint32_t nNodes2 = routers.GetN();
+    // for (uint32_t i = 0; i < nNodes2; ++i)
+    // {
+    //     Ptr<Node> p = routers.Get(i);
+    //     std::cout << "Node " << p->GetId() << std::endl;  // some Node method
+    // }
 
     // Create the point-to-point link helpers
     PointToPointHelper bottleneckLink;
-    bottleneckLink.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    bottleneckLink.SetDeviceAttribute("DataRate", StringValue("1000Mbps"));
     bottleneckLink.SetChannelAttribute("Delay", StringValue("10ms"));
 
     PointToPointHelper edgeLink;
-    edgeLink.SetDeviceAttribute("DataRate", StringValue("1000Mbps"));
+    edgeLink.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
     edgeLink.SetChannelAttribute("Delay", StringValue("5ms"));
 
+    PointToPointHelper edgeLink1;
+    edgeLink1.SetDeviceAttribute("DataRate", StringValue("1000Mbps"));
+    edgeLink1.SetChannelAttribute("Delay", StringValue("5ms"));
+
     // Create NetDevice containers
-    NetDeviceContainer senderEdge = edgeLink.Install(sender.Get(0), routers.Get(0));
+    // Modifeid
+    NetDeviceContainer sender0Edge = edgeLink.Install(senders.Get(0), routers.Get(0));
+    NetDeviceContainer sender1Edge = edgeLink.Install(senders.Get(1), routers.Get(0));
+    NetDeviceContainer sender2Edge = edgeLink.Install(senders.Get(2), routers.Get(0));
     NetDeviceContainer r1r2 = bottleneckLink.Install(routers.Get(0), routers.Get(1));
-    NetDeviceContainer receiverEdge = edgeLink.Install(routers.Get(1), receiver.Get(0));
+    NetDeviceContainer receiverEdge = edgeLink1.Install(routers.Get(1), receiver.Get(0));
 
     // Install Stack
+    // Modifeid
     InternetStackHelper internet;
-    internet.Install(sender);
+    internet.Install(senders);
     internet.Install(receiver);
     internet.Install(routers);
 
@@ -207,44 +318,57 @@ main(int argc, char* argv[])
         tch.SetQueueLimits("ns3::DynamicQueueLimits", "HoldTime", StringValue("1000ms"));
     }
 
-    tch.Install(senderEdge);
+    // Modifeid
+    tch.Install(sender0Edge);
+    tch.Install(sender1Edge);
+    tch.Install(sender2Edge);
     tch.Install(receiverEdge);
 
     // Assign IP addresses
     Ipv4AddressHelper ipv4;
-    ipv4.SetBase("10.0.0.0", "255.255.255.0");
+    ipv4.SetBase("10.1.1.0", "255.255.255.0");
 
     Ipv4InterfaceContainer i1i2 = ipv4.Assign(r1r2);
 
-    ipv4.NewNetwork();
-    Ipv4InterfaceContainer is1 = ipv4.Assign(senderEdge);
+    ipv4.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer is0 = ipv4.Assign(sender0Edge);
 
-    ipv4.NewNetwork();
+    ipv4.SetBase("10.1.3.0", "255.255.255.0");
+    Ipv4InterfaceContainer is1 = ipv4.Assign(sender1Edge);
+
+    ipv4.SetBase("10.1.4.0", "255.255.255.0");
+    Ipv4InterfaceContainer is2 = ipv4.Assign(sender2Edge);
+
+    ipv4.SetBase("10.1.5.0", "255.255.255.0");
     Ipv4InterfaceContainer ir1 = ipv4.Assign(receiverEdge);
 
     // Populate routing tables
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
     // Select sender side port
-    uint16_t port = 50001;
+    uint16_t port_0 = 50001;
 
-    // Install application on the sender
-    BulkSendHelper source("ns3::TcpSocketFactory", InetSocketAddress(ir1.GetAddress(1), port));
-    source.SetAttribute("MaxBytes", UintegerValue(0));
-    ApplicationContainer sourceApps = source.Install(sender.Get(0));
-    sourceApps.Start(Seconds(0.1));
-    // Hook trace source after application starts
-    Simulator::Schedule(Seconds(0.1) + MilliSeconds(1), &TraceCwnd, 0, 0);
-    sourceApps.Stop(stopTime);
+    for (uint16_t i = 0; i < 3; i++)
+    {
+        port_0 = port_0 + i;
+        // Install application on the sender
+        BulkSendHelper source0("ns3::TcpSocketFactory", InetSocketAddress(ir1.GetAddress(1), port_0));
+        source0.SetAttribute("MaxBytes", UintegerValue(0));
+        ApplicationContainer sourceApps0 = source0.Install(senders.Get(i));
+        sourceApps0.Start(Seconds(0.1));
+        // Hook trace source after application starts // tracing sender 0
+        Simulator::Schedule(Seconds(0.1) + MilliSeconds(1), &TraceCwnd, i, 0);
+        sourceApps0.Stop(stopTime);
 
-    // Install application on the receiver
-    PacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
-    ApplicationContainer sinkApps = sink.Install(receiver.Get(0));
-    sinkApps.Start(Seconds(0.0));
-    sinkApps.Stop(stopTime);
+        // Install application on the receiver
+        PacketSinkHelper sink0("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port_0));
+        ApplicationContainer sinkApps0 = sink0.Install(receiver.Get(0));
+        sinkApps0.Start(Seconds(0.0));
+        sinkApps0.Stop(stopTime);
+    }
 
     // Create a new directory to store the output of the program
-    dir = "llm-results_unique/";
+    dir = "multi_sender_results/";
     std::string dirToSave = "mkdir -p " + dir;
     if (system(dirToSave.c_str()) == -1)
     {
@@ -292,12 +416,17 @@ main(int argc, char* argv[])
     // Check if throughput.dat exist, if so, truncate it
     std::ofstream thr2(dir + "/throughput.dat", std::ios::out | std::ios::trunc);
     thr2.close();
+
+    // Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon.GetClassifier());
+
     Simulator::Schedule(Seconds(0 + 0.000001), &TraceThroughput, monitor);
 
     std::cout << "Schedule RTT tracing" << std::endl;
 
     // Schedule RTT tracing
     Simulator::Schedule(Seconds(0.2) + MilliSeconds(1), &TraceRtt, 0, 0);
+    Simulator::Schedule(Seconds(0.2) + MilliSeconds(1), &TraceRtt, 1, 0);
+    Simulator::Schedule(Seconds(0.2) + MilliSeconds(1), &TraceRtt, 2, 0);
 
     Simulator::Stop(stopTime + TimeStep(1));
     Simulator::Run();
